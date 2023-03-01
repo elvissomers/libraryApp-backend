a52ebdc7c6af937899b97dc21385c86321e2cbdf
elvissomers <elvissomers98@gmail.com>
2023-03-01 13:40:33 +0100

    Update userController.java
    removed unused import

--- src/main/java/wt/bookstore/backend/controllers/userController.java
+++ src/main/java/wt/bookstore/backend/controllers/userController.java
@@ -3,7 +3,6 @@ package wt.bookstore.backend.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import wt.bookstore.backend.domains.Book;
 import wt.bookstore.backend.domains.Loan;
 import wt.bookstore.backend.domains.Reservation;
 import wt.bookstore.backend.domains.User;
