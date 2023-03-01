fb13b87117cc232263670bdbc5756e8af62543ed
elvissomers <elvissomers98@gmail.com>
2023-03-01 10:47:02 +0100

    Find copies by book
    Implemented method & endpoint to find all copies of a specific book

--- src/main/java/wt/bookstore/backend/controllers/bookController.java
+++ src/main/java/wt/bookstore/backend/controllers/bookController.java
@@ -3,7 +3,9 @@ package wt.bookstore.backend.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import wt.bookstore.backend.domains.Book;
+import wt.bookstore.backend.domains.Copy;
 import wt.bookstore.backend.repository.IBookRepository;
+import wt.bookstore.backend.repository.ICopyRepository;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,35 +15,40 @@ import java.util.Optional;
 public class bookController {
 
     @Autowired
-    private IBookRepository repository;
+    private IBookRepository bookRepository;
+    
+    @Autowired
+    private ICopyRepository copyRepository;
 
     @RequestMapping(value = "book", method = RequestMethod.GET)
     public List<Book> findAll() {
-        return repository.findAll();
+        return bookRepository.findAll();
     }
 
     @RequestMapping(value="book/create", method = RequestMethod.POST)
     public void create(@RequestBody Book book) {
-        repository.save(book);
+        bookRepository.save(book);
     }
 
     @RequestMapping(value = "book/{id}", method = RequestMethod.PUT)
     public void update(@PathVariable long id, @RequestBody Book book) {
-        Optional<Book> optional = repository.findById(id);
+        Optional<Book> optional = bookRepository.findById(id);
         optional.get().setTitle(book.getTitle());
         optional.get().setISBN(book.getISBN());
-        //optional.get().setKeywords(book.getKeywords());
         optional.get().setAuthor(book.getAuthor());
-        //optional.get().setCopyList(book.getCopyList());
-        //optional.get().setReservationList(book.getReservationList());
 
 
-        repository.save(optional.get());
+        bookRepository.save(optional.get());
     }
 
     @RequestMapping(value = "book/{id}", method = RequestMethod.DELETE)
     public void delete(@PathVariable long id) {
-        repository.deleteById(id);
+        bookRepository.deleteById(id);
+    }
+    
+    @RequestMapping(value = "book/{id}/copies", method = RequestMethod.GET)
+    public List<Copy> findCopies(@PathVariable long id){
+    	return copyRepository.findByBookId(id);
     }
 
     
