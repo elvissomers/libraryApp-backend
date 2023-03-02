f6cb5aba188312e5df3ca380fe22c40dce99a525
elvissomers <elvissomers98@gmail.com>
2023-03-02 10:10:14 +0100

    docs

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"wt.bookstore.backend.controllers"}];updateSearchResults();
---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"wt.bookstore.backend.controllers","l":"bookController"}];updateSearchResults();
