2849faf56df13f99b14e4b2a471cb202806b1ef6
elvissomers <elvissomers98@gmail.com>
2023-03-09 08:37:47 +0100

    Restored discarded changes

--- src/main/java/wt/bookstore/backend/domains/Book.java
+++ src/main/java/wt/bookstore/backend/domains/Book.java
@@ -25,6 +25,13 @@ public class Book {
 	@Column(nullable = false, length = 100)
 	private String author;
 
+	@ManyToMany()
+	@JoinTable(
+			name = "book_keywords",
+			joinColumns = @JoinColumn(name = "book_id"),
+			inverseJoinColumns = @JoinColumn(name = "keyword_id"))
+	private List<Keyword> keywords = new ArrayList<>();
+
 	@OneToMany(mappedBy = "book", orphanRemoval = true)
 	private List<Copy> copies = new ArrayList<>();
 
@@ -86,6 +93,18 @@ public class Book {
 
 	public void setReservations(List<Reservation> reservations) {
 		this.reservations = reservations;
+	}
+
+	public List<Keyword> getKeywords() {
+		return keywords;
+	}
+
+	public void setKeywords(List<Keyword> keywords) {
+		this.keywords = keywords;
+	}
+
+	public void addKeyword(Keyword keyword) {
+		keywords.add(keyword);
 	}
 
 	/**
