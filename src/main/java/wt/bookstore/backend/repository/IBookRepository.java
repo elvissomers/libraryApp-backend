edf825a32347164611d534a0aeb8ed7d6347b94d
Anwar <anwar.sakkali.kk@gmail.com>
2023-03-10 13:59:45 +0100

    fix import

--- src/main/java/wt/bookstore/backend/repository/IBookRepository.java
+++ src/main/java/wt/bookstore/backend/repository/IBookRepository.java
@@ -1,5 +1,5 @@
 package wt.bookstore.backend.repository;
-
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import wt.bookstore.backend.domains.Book;
@@ -10,4 +10,5 @@ public interface IBookRepository extends JpaRepository<Book, Long> {
 
 
     List<Book> findByTitleContainingOrAuthorContaining(String title, String Author, Pageable pageable);
+    
 }
